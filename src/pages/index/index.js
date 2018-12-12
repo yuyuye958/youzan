7b564d010f605c7fd75bbe31d1147352c09a884e
Allen <yuyuye958@qq.com>
2018-12-12 17:13:20 +0800

    实现触底加载商品列表，底部导航改为组件

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,21 +1,53 @@
 import 'css/common.css'
 import './index.css'
+
 import Vue from 'vue'
 import axios from 'axios'
 import url from 'js/api.js'
+import Foot from 'components/Foot'
 
+import {InfiniteScroll} from 'mint-ui'
+
+Vue.use(InfiniteScroll)
 
 let app = new Vue({
   el: '#app',
   data: {
     lists: null,
+    loading: false,
+    pageNum: 1,
+    pageSize: 6,
+    allLoaded: false
+  },
+  components: {
+    Foot
   },
   created() {
-    axios.get(url.hotLists, {
-      pageNum: 1,
-      pageSize: 6
-    }).then((res) => {
-      this.lists = res.data.lists
-    })
+    this.getLists()
+  },
+  methods: {
+    getLists() {
+      if (this.allLoaded) {
+        return
+      }
+      this.loading = true
+      axios.get(url.hotLists, {
+        pageNum: this.pageNum,
+        pageSize: this.pageSize
+      }).then((res) => {
+        let curLists = res.data.lists
+        // 判断所有数据是否加载完毕
+        if (curLists.length < this.pageSize) {
+          this.allLoaded = true
+        }
+        if (this.lists) {
+          this.lists = this.lists.concat(curLists)
+        } else {
+          this.lists = curLists
+        }
+        this.pageNum++
+        this.loading = false
+      })
+    }
   }
 })
