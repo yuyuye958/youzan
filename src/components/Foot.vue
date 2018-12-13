519e9d2746fba54692e95be69a29a6d097e5cafb
Allen <yuyuye958@qq.com>
2018-12-14 02:18:15 +0800

    重写Foot组件，实现多页面跳转

--- src/components/Foot.vue
+++ src/components/Foot.vue
@@ -1,25 +1,52 @@
 <template>
   <div class="bottom-nav">
     <ul>
-      <li class="active"><a href="index.html"><i class="icon-home"></i>
-        <div>有赞</div>
-      </a></li>
-      <li><a href="https://maijia.youzan.com/mars/category"><i class="icon-category"></i>
-        <div>分类</div>
-      </a></li>
-      <li><a href="https://h5.youzan.com/v2/trade/cart?f_platform=yzapp&amp;source=yzapp"><i class="icon-cart"></i>
-        <div>购物车</div>
-      </a></li>
-      <li><a href="https://h5.youzan.com/v2/buyer/member"><i class="icon-user"></i>
-        <div>我</div>
-      </a></li>
+      <li :class="{active: index === curIndex}" v-for="(list, index) in navConfig" @click="changeNav(list, index)"
+          :key="index">
+        <a>
+          <i :class="list.icon"></i>
+          <div>{{list.name}}</div>
+        </a>
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
+  import qs from 'qs'
+
+  let {index} = qs.parse(location.search.substring(1))  //获取当前url的查询参数中index的值，该值为字符串
+  let navConfig = [{
+    name: '有赞',
+    icon: 'icon-home',
+    href: 'index.html'
+  }, {
+    name: '分类',
+    icon: 'icon-category',
+    href: 'category.html'
+  }, {
+    name: '购物车',
+    icon: 'icon-cart',
+    href: 'cart.html'
+  }, {
+    name: '我',
+    icon: 'icon-user',
+    href: 'member.html'
+  }]
+
   export default {
-    name: 'Foot'
+    name: 'Foot',
+    data() {
+      return {
+        navConfig,
+        curIndex: parseInt(index) || 0  // 若index不存在，则curIndex为0，即首页
+      }
+    },
+    methods: {
+      changeNav(list, index) {
+        location.href = `${list.href}?index=${index}`
+      }
+    }
   }
 </script>
 
---
+++ src/pages/cart/cart.js
@@ -0,0 +1,8 @@
+import './cart_base.css'
+import './cart_trade.css'
+import './cart.css'
+
+import Vue from 'vue'
+import axios from 'axios'
+import url from 'js/api.js'
+import Foot from 'components/Foot'
